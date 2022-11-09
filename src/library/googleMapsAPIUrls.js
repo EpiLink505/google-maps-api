1d2c1d290b88367099c443c7a98ecb107839fb79
Peter Pham <ppham5472@gmail.com>
2022-11-09 11:29:00 -0700

    11/9 cors 4

--- src/library/googleMapsAPIUrls.js
+++ src/library/googleMapsAPIUrls.js
@@ -1,14 +1,9 @@
-/*
-export const GOOGLE_MAPS_API_DIRECTIONS_URL =
-  "https://distancetorestaurants.netlify.app/pathx/directions/json";
-export const GOOGLE_MAPS_API_PLACEID_URL =
-  "https://distancetorestaurants.netlify.app/pathx/place/findplacefromtext/json";
-export const GOOGLE_MAPS_API_SEARCHNEARBY_URL =
-  "https://distancetorestaurants.netlify.app/pathx/place/nearbysearch/json";
-export const GOOGLE_MAPS_API_PLACE_URL =
-  "https://distancetorestaurants.netlify.app/pathx/place/details/json";
-  */
+export const GOOGLE_MAPS_API_DIRECTIONS_URL = "/api/directions/json";
+export const GOOGLE_MAPS_API_PLACEID_URL = "/api/place/findplacefromtext/json";
+export const GOOGLE_MAPS_API_SEARCHNEARBY_URL = "/api/place/nearbysearch/json";
+export const GOOGLE_MAPS_API_PLACE_URL = "/api/place/details/json";
 
+/*
 export const GOOGLE_MAPS_API_DIRECTIONS_URL =
   "https://maps.googleapis.com/maps/api/directions/json";
 export const GOOGLE_MAPS_API_PLACEID_URL =
@@ -17,3 +12,4 @@ export const GOOGLE_MAPS_API_SEARCHNEARBY_URL =
   "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
 export const GOOGLE_MAPS_API_PLACE_URL =
   "https://maps.googleapis.com/maps/api/place/details/json";
+  */
