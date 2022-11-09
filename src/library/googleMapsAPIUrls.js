fc6b41641ed8746bcf0135fa95bcfbfa5b823e28
Peter Pham <ppham5472@gmail.com>
2022-11-09 11:09:13 -0700

    11/9 cors _headers

--- src/library/googleMapsAPIUrls.js
+++ src/library/googleMapsAPIUrls.js
@@ -1,3 +1,4 @@
+/*
 export const GOOGLE_MAPS_API_DIRECTIONS_URL =
   "https://distancetorestaurants.netlify.app/pathx/directions/json";
 export const GOOGLE_MAPS_API_PLACEID_URL =
@@ -6,9 +7,9 @@ export const GOOGLE_MAPS_API_SEARCHNEARBY_URL =
   "https://distancetorestaurants.netlify.app/pathx/place/nearbysearch/json";
 export const GOOGLE_MAPS_API_PLACE_URL =
   "https://distancetorestaurants.netlify.app/pathx/place/details/json";
+  */
 
-/*
-  export const GOOGLE_MAPS_API_DIRECTIONS_URL =
+export const GOOGLE_MAPS_API_DIRECTIONS_URL =
   "https://maps.googleapis.com/maps/api/directions/json";
 export const GOOGLE_MAPS_API_PLACEID_URL =
   "https://maps.googleapis.com/maps/api/place/findplacefromtext/json";
@@ -16,4 +17,3 @@ export const GOOGLE_MAPS_API_SEARCHNEARBY_URL =
   "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
 export const GOOGLE_MAPS_API_PLACE_URL =
   "https://maps.googleapis.com/maps/api/place/details/json";
-  */
